6d3e7d3841250cf8055715d996c92d3d3e1ea1e3
Ignacio Díaz <ignaciodiaz@computador-de-nachodiaz.local>
2021-02-05 03:41:20 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+var http = require('http');
+var servidor = http.createServer(function(petición, respuesta){
+    respuesta.writeHead(200, {'Content-Type' : 'text/html'});
+    respuesta.write("bienvenido a www.weilaystudio.com");
+    console.log("petición web");
+})
+
+servidor.listen(80)
+
+console.log('Escuchando al puerto 80')
