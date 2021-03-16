e8ba568eb8e070cea0782dc3b5e5f991ba6e93fc
weilaystudio.com <ignaciosergiodiaz@gmail.com>
2021-03-15 23:40:17 -0300

    add static route

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
-const port = 80 
+const port = 80 ;
 
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.configure(function(){
+
+  app.use(express.static(__dirname + '/public'));
+  app.use(express.json());
+  app.use(express.urlencoded({extended: true}));
+
+});
+
+app.use(cors());
 
 app.get('/', ( req, res) => {
-    res.status(200).send(`<h1>Sitio Web en Construcción 2021 </h1> `)
-})
+    res.status(200).send(`<h1>Sitio Web en Construcción 2021 </h1> `);
+});
 
-app.listen(port, async ()=>{
-  await  console.log(`Servidor Se esta esuchando en el puerto 80`)
-})
+app.listen(port, async () => {
+  await  console.log(`Servidor Se esta esuchando en el puerto 80`);
+});
