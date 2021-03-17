96d9e86d3022019819ca9ffa6df56e3bc1c987d1
weilaystudio.com <ignaciosergiodiaz@gmail.com>
2021-03-17 18:39:13 -0300

    add h1 in contacto page

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.get('/quienes-somos', (req, res) => {
 })
 
 app.get('/contacto', (req, res) => {
-  return res.status(200).send('Hola te responderé a la brevedad');
+  return res.status(200).send(`<h1>Hola te responderé a la brevedad</h1>`);
 })
 
 app.listen(port, async () => {
