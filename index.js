d08fc2aa74adaccc4ac8ea9d1ada9b7cfba14b84
weilaystudio.com <ignaciosergiodiaz@gmail.com>
2021-04-02 23:59:51 -0300

    update port 3000

--- index.js
+++ index.js
@@ -2,11 +2,7 @@ const express = require('express');
 const server = express();
 
 //port 
-<<<<<<< HEAD
-const port = 8080;
-=======
-const port = 3000 ;
->>>>>>> 1cfd9eeba439603fa7495338937b8aee03fe7316
+const port = 3000 ; 
 
 server.get('/', (req, res)=>{
    return res.status(200).send(`<h1> Bienvenido a WeilayStudio <h1>`);
